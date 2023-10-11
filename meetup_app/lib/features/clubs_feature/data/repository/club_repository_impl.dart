bd5cd9ebe2eceb8ed8768a2c87e0a6556fdb9336
I-WOObin-I <lubin07000@gmail.com>
2023-10-12 00:51:05 +0200

    added api response simulation

--- meetup_app/lib/features/clubs_feature/data/repository/club_repository_impl.dart
+++ meetup_app/lib/features/clubs_feature/data/repository/club_repository_impl.dart
@@ -12,25 +12,44 @@ class ClubRepositoryImpl implements ClubRepository {
   @override
   Future<DataState<List<ClubModel>>> getClubs() async {
     try {
-      final httpResponse = await _clubsApiService.getClubs(
-        apiKey: clubsAPIKey,
-        sportType: "sample sport",
-        country: "sample country",
-        city: "sample city",
-      );
+      List<ClubModel> data = [
+        const ClubModel(id: "1", name: "Club 1"),
+        const ClubModel(id: "2", name: "Club 2"),
+        const ClubModel(id: "3", name: "Club 3"),
+        const ClubModel(id: "4", name: "Club 4"),
+        const ClubModel(id: "5", name: "Club 5"),
+      ];
 
-      if (httpResponse.response.statusCode == 200) {
-        return DataSuccess(httpResponse.data);
-      } else {
-        return DataFailed(DioException(
-          error: httpResponse.response.statusMessage,
-          response: httpResponse.response,
-          type: DioExceptionType.badResponse,
-          requestOptions: httpResponse.response.requestOptions,
-        ));
-      }
+      await Future.delayed(const Duration(seconds: 2));
+
+      return DataSuccess(data);
     } on DioException catch (e) {
       return DataFailed(e);
     }
   }
+
+  // @override
+  // Future<DataState<List<ClubModel>>> getClubs() async {
+  //   try {
+  //     final httpResponse = await _clubsApiService.getClubs(
+  //       apiKey: clubsAPIKey,
+  //       sportType: "sample sport",
+  //       country: "sample country",
+  //       city: "sample city",
+  //     );
+
+  //     if (httpResponse.response.statusCode == 200) {
+  //       return DataSuccess(httpResponse.data);
+  //     } else {
+  //       return DataFailed(DioException(
+  //         error: httpResponse.response.statusMessage,
+  //         response: httpResponse.response,
+  //         type: DioExceptionType.badResponse,
+  //         requestOptions: httpResponse.response.requestOptions,
+  //       ));
+  //     }
+  //   } on DioException catch (e) {
+  //     return DataFailed(e);
+  //   }
+  // }
 }
